496cfbecf6137f0f418ea9608571a41f0f258c4b
caioketo <caioketo@live.com>
2013-06-18 17:30:14 -0300

    c

---
+++ WERP/Controllers/ProdutosController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WERP.Models;
+
+namespace WERP.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private WERPContext db = new WERPContext();
+
+        //
+        // GET: /Produtos/
+
+        public ActionResult Index()
+        {
+            return View(db.ProdutoModels.ToList());
+        }
+
+        //
+        // GET: /Produtos/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            ProdutoModel produtomodel = db.ProdutoModels.Find(id);
+            if (produtomodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtomodel);
+        }
+
+        //
+        // GET: /Produtos/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Produtos/Create
+
+        [HttpPost]
+        public ActionResult Create(ProdutoModel produtomodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProdutoModels.Add(produtomodel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(produtomodel);
+        }
+
+        //
+        // GET: /Produtos/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            ProdutoModel produtomodel = db.ProdutoModels.Find(id);
+            if (produtomodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtomodel);
+        }
+
+        //
+        // POST: /Produtos/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(ProdutoModel produtomodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produtomodel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produtomodel);
+        }
+
+        //
+        // GET: /Produtos/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            ProdutoModel produtomodel = db.ProdutoModels.Find(id);
+            if (produtomodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtomodel);
+        }
+
+        //
+        // POST: /Produtos/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProdutoModel produtomodel = db.ProdutoModels.Find(id);
+            db.ProdutoModels.Remove(produtomodel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WERP/Models/ProdutoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WERP.Models
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set; }
+        public string Desc { get; set; }
+    }
+}
